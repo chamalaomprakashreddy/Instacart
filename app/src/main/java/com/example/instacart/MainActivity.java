0e77df02a8e2b23e6d27c9480ff0922f3af168bd
Siddhartha_Sangani <156238859+SiddharthaSangani@users.noreply.github.com>
2024-10-12 07:42:44 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/instacart/MainActivity.java
+++ app/src/main/java/com/example/instacart/MainActivity.java
@@ -26,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        Button createBTN = findViewById(R.id.createBTN);
+        createBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CreateAccountActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
