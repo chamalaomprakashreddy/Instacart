2d79b838fd57d71409b0f1f0c7c3705417999b51
Rakesh Kusa <156496348+reddy024@users.noreply.github.com>
2024-11-01 23:25:10 -0500

    Rakesh(S572126)
    changes done

---
+++ app/src/main/java/com/example/instacart/OrderItem.java
@@ -0,0 +1,67 @@
+package com.example.instacart;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class OrderItem implements Parcelable {
+    private String name;
+    private double price;
+    private int quantity;
+    private String imageUrl;
+
+    public OrderItem(String name, double price, int quantity, String imageUrl) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.imageUrl = imageUrl;
+    }
+
+    protected OrderItem(Parcel in) {
+        name = in.readString();
+        price = in.readDouble();
+        quantity = in.readInt();
+        imageUrl = in.readString();
+    }
+
+    public static final Creator<OrderItem> CREATOR = new Creator<OrderItem>() {
+        @Override
+        public OrderItem createFromParcel(Parcel in) {
+            return new OrderItem(in);
+        }
+
+        @Override
+        public OrderItem[] newArray(int size) {
+            return new OrderItem[size];
+        }
+    };
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeDouble(price);
+        dest.writeInt(quantity);
+        dest.writeString(imageUrl);
+    }
+}
+
