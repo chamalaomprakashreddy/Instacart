bc7371560f404f3bf1c8956901fa5e04384b691e
Siddhartha_Sangani <156238859+SiddharthaSangani@users.noreply.github.com>
2024-11-04 02:33:07 +0530

    siddhartha
    firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
 
+
 }
 
 
@@ -35,16 +36,8 @@ android {
 
 
 dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
-    implementation( "com.google.firebase:firebase-auth:22.3.1")
-    implementation("com.google.firebase:firebase-analytics")
     implementation ("androidx.appcompat:appcompat:1.6.1")
-    implementation ("com.google.firebase:firebase-firestore:24.11.0")
     implementation ("androidx.annotation:annotation:1.7.1")
-    implementation ("com.google.firebase:firebase-firestore:24.11.0")
-    implementation ("com.google.firebase:firebase-database:20.0.0")
-    implementation ("com.google.firebase:firebase-firestore:23.0.3")
-    implementation ("com.google.firebase:firebase-firestore:23.0.4")
     implementation ("com.github.bumptech.glide:glide:4.14.2")
     implementation ("com.squareup.picasso:picasso:2.71828")
     implementation ("androidx.navigation:navigation-fragment-ktx:2.7.7")
@@ -52,6 +45,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth:23.1.0")
+    implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation("com.google.firebase:firebase-firestore:25.1.1")
+    implementation("com.google.firebase:firebase-storage:21.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
